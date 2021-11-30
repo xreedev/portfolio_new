17e3f0e3e6d8a2418b5dc9d01a5262a82670ea77
sreedev <xreedev@gmail.com>
2021-11-30 20:06:33 +0530

    First Commit

---
+++ app.jsx
@@ -0,0 +1,22 @@
+
+var baseUrl = "https://api.coinranking.com/v2/coins";
+var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+var apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+var apiUrl = `${proxyUrl}${baseUrl}`;
+console.log(apiUrl);
+import React from "react";
+import { ReactDOM } from "react";
+
+class app extends Component{
+    state={
+        count: 0
+    }
+};
+
+render() {
+    return {
+        <h1> HELLO WORLD </h1>
+
+    };r1t
+} 
